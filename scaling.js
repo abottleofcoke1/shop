27ab7f1785e87cfee5aeb5b4870995a822d63b4b
matt <mattyysekk@gmail.com>
2020-12-21 14:43:36 +0100

    Create scaling.js

---
+++ scaling.js
@@ -0,0 +1,72 @@
+if (screen.width >= 1680) {
+	
+	document.write("<style>body{max-height: 800px; max-width: 1920;}.boxYeezy{margin-left: 189px;}.header{margin-left: -10px;}.BetterVis{margin-top: 145px;}</style>")
+}
+
+function changeColor(id)
+{
+ 
+  document.getElementById(id).style.backgroundColor = "#ebebeb";
+  document.getElementById(id).style.border = "3px solid #bdbdbd";
+  document.getElementById(id).style.boxShadow = "12px 12px #2e2e2e";
+}
+
+function changeColorBorder(id)
+{
+ 
+  document.getElementById(id).style.border = "3px solid #d9d9d9";
+}             
+
+function changeColorTwo(id)
+{
+	
+	document.getElementById(id).style.backgroundColor = "#ebebeb";
+	document.getElementById(id).style.border = "3px solid #bdbdbd";
+    document.getElementById(id).style.boxShadow = "12px 12px #2e2e2e";
+}
+
+function changeColorBorderTwo(id)
+{
+ 
+  document.getElementById(id).style.border = "3px solid #d9d9d9";
+}
+
+function changeColorThree(id)
+{
+	
+	document.getElementById(id).style.backgroundColor = "#ebebeb";
+	document.getElementById(id).style.border = "3px solid #bdbdbd";
+    document.getElementById(id).style.boxShadow = "12px 12px #2e2e2e";
+}   
+
+function changeColorBorderThree(id)
+{
+ 
+  document.getElementById(id).style.border = "3px solid #d9d9d9";
+}
+
+function changeColorFour(id)
+{
+	
+	document.getElementById(id).style.backgroundColor = "#ebebeb";
+	document.getElementById(id).style.border = "3px solid #bdbdbd";
+    document.getElementById(id).style.boxShadow = "12px 12px #2e2e2e";
+}   
+
+function changeColorBorderFour(id)
+{
+ 
+  document.getElementById(id).style.border = "3px solid #d9d9d9";
+}
+
+function changeBackground(id)
+{
+ 
+  document.getElementById(id).style.backgroundColor = "#1a1a1a";
+}
+
+function darkmode(id) 
+{
+	
+	document.getElementById(id).style.backgroundColor = "white";
+}
